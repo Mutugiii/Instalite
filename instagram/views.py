b9e871a5ca3513323988ab49364844a8c408733e
mutugiii <mutugimutuma@gmail.com>
2020-03-07 23:00:39 +0300

    Implement first template-view action

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, loader
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    '''View Function for the Main page'''
+    template = loader.get_template('index.html')
+    context = {
+        'message': 'Welcome'
+    }
+    return HttpResponse(template.render(context, request))
