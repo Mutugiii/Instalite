bcb1be55616febc5439c7c1250493b2dcffac417
mutugiii <mutugimutuma@gmail.com>
2020-03-08 14:40:29 +0300

    Write tests for Profile & Post Model

--- instagram/models.py
+++ instagram/models.py
@@ -17,20 +17,13 @@ class CrudMethods:
             setattr(self,key,value)
             self.save()
 
-class Comment(models.Model, CrudMethods):
-    '''Method to allow user comments'''
-    comments = models.TextField()
-
-    def __str__(self):
-        return self.comments
-
 class Profile(models.Model, CrudMethods):
     '''Model class for the user and his profile'''
     username = models.CharField(max_length=30)
     bio = models.TextField(blank=True)
     profile_photo = models.ImageField(upload_to='images/', blank=True)
-    follower = models.ForeignKey(User, related_name='following', blank=True, on_delete=models.CASCADE)
-    following = models.ForeignKey(User, related_name='followers', blank=True, on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_name='following', blank=True, null=True, on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', blank=True, null=True, on_delete=models.CASCADE)
     joined = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -42,21 +35,28 @@ class Profile(models.Model, CrudMethods):
 class Post(models.Model, CrudMethods):
     '''Models Class to implement publishing of new posts/content'''
     post_caption = models.TextField()
-    post = models.CharField
-    image_link = models.ImageField(upload_to='images/', blank=True)
+    post_image = models.ImageField(upload_to='images/', blank=True)
     user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    likes = models.IntegerField()
-    comments = models.ManyToManyField(Comment)
+    likes = models.IntegerField(default=0)
     published = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.post
         
+class Comment(models.Model, CrudMethods):
+    '''Method to allow user comments'''
+    comment = models.TextField()
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comments
+
 class Like(models.Model, CrudMethods):
     '''Model class to allow users to like photos'''
+    value = models.IntegerField(blank=True)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    value = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.value
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+from .models import Comment, Profile, Post, Like
 
-# Create your tests here.
+class TestProfile(TestCase):
+    '''Test Class to test the Profile Class'''
+    def setUp(self)-> None:
+        '''To set up test class before running every test case'''
+        self.test_profile = Profile(username = 'Test', bio = 'This is just a test user')
+    
+    def tearDown(self) -> None:
+        '''To clean up after running every testcase'''
+        Profile.objects.all().delete()
+
+    def test_isinstance(self):
+        '''To test if object is an instance of Class'''
+        self.assertTrue(isinstance(self.test_profile, Profile))
+
+    def test_save_profile(self):
+        '''To test saving the profile'''
+        self.test_profile.save_class()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 1)
+
+    def test_delete_profile(self):
+        '''To test deleting profile'''
+        self.test_profile.save_class()
+        self.test_profile.delete_class()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
+    def test_update_profile(self):
+        '''Test the profile updating'''
+        self.test_profile.save_class()
+        self.test_profile.update_class(username = 'newTest')
+        self.assertEqual(self.test_profile.username, 'newTest')
+
+class TestPost(TestCase):
+    '''Test Class to test the Post Class'''
+    def setUp(self) -> None:
+        '''To set up test class before running every test case'''
+        self.test_profile = Profile(username = 'Test', bio = 'This is just a test user')
+        self.test_profile.save_class()
+        self.test_post = Post(post_caption='What a time to be alive',user_profile = self.test_profile)
+
+    def tearDown(self) -> None:
+        '''To clean up after every test case'''
+        Post.objects.all().delete()
+
+    def test_isinstance(self):
+        '''To test if object is an instance of Class'''
+        self.assertTrue(isinstance(self.test_post, Post))
+
+    def test_save_post(self):
+        '''To test saving the post'''
+        self.test_post.save_class()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) == 1)
+
+    def test_delete_post(self):
+        '''To test deleting post'''
+        self.test_post.save_class()
+        self.test_post.delete_class()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) == 0)
+
+    def test_update_post(self):
+        '''Test the post updating'''
+        self.test_post.save_class()
+        self.test_post.update_class(post_caption = 'You only live once')
+        self.assertEqual(self.test_post.post_caption, 'You only live once')
